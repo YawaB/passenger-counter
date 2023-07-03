905d64724d709783e03c2362290f0d860a1d280e
YawaB <mawuviwobrinda@gmail.com>
2023-07-03 01:42:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let count = 0;
+document.getElementById("increment-btn").addEventListener("click", function(){
+    count++;
+    document.getElementById("count-el").innerText = count;
+});
+
+document.getElementById("decrement-btn").addEventListener("click", function(){
+    if(count > 0){
+        count--;
+        document.getElementById("count-el").innerText = count;
+    }
+});
+
+document.getElementById("save-btn").addEventListener("click", function(){
+    let countStr = count + " - ";
+    document.getElementById("save-el").textContent += countStr;
+    count = 0;
+    document.getElementById("count-el").innerHTML = count;
+});
+
+console.log("count: ", count);
